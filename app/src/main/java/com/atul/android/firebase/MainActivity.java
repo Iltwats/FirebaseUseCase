21db3365c7c2f9b4143f9697e6d154b0cac225a0
Iltwats <2012atulsharma@gmail.com>
2020-11-23 12:55:02 +0530

    Added query on Firestore

--- app/src/main/java/com/atul/android/firebase/MainActivity.java
+++ app/src/main/java/com/atul/android/firebase/MainActivity.java
@@ -12,8 +12,10 @@ import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.Timestamp;
+import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 import com.google.firebase.firestore.SetOptions;
@@ -35,7 +37,81 @@ public class MainActivity extends AppCompatActivity {
         //addUserData();
         //setDocument();
         //dataTypes();
-        getdata();
+        //getdata();
+        performQueries();
+    }
+
+    private void performQueries() {
+        CollectionReference cities = db.collection("cities");
+
+        Map<String, Object> data1 = new HashMap<>();
+        data1.put("name", "San Francisco");
+        data1.put("state", "CA");
+        data1.put("country", "USA");
+        data1.put("capital", false);
+        data1.put("population", 860000);
+        data1.put("regions", Arrays.asList("west_coast", "norcal"));
+        cities.document("SF").set(data1);
+
+        Map<String, Object> data2 = new HashMap<>();
+        data2.put("name", "Los Angeles");
+        data2.put("state", "CA");
+        data2.put("country", "USA");
+        data2.put("capital", false);
+        data2.put("population", 3900000);
+        data2.put("regions", Arrays.asList("west_coast", "socal"));
+        cities.document("LA").set(data2);
+
+        Map<String, Object> data3 = new HashMap<>();
+        data3.put("name", "Washington D.C.");
+        data3.put("state", null);
+        data3.put("country", "USA");
+        data3.put("capital", true);
+        data3.put("population", 680000);
+        data3.put("regions", Arrays.asList("east_coast"));
+        cities.document("DC").set(data3);
+
+        Map<String, Object> data4 = new HashMap<>();
+        data4.put("name", "Tokyo");
+        data4.put("state", null);
+        data4.put("country", "Japan");
+        data4.put("capital", true);
+        data4.put("population", 9000000);
+        data4.put("regions", Arrays.asList("kanto", "honshu"));
+        cities.document("TOK").set(data4);
+
+        Map<String, Object> data5 = new HashMap<>();
+        data5.put("name", "Beijing");
+        data5.put("state", null);
+        data5.put("country", "China");
+        data5.put("capital", true);
+        data5.put("population", 21500000);
+        data5.put("regions", Arrays.asList("jingjinji", "hebei"));
+        cities.document("BJ").set(data5);
+
+        // Create a reference to the cities collection
+        CollectionReference citiesRef = db.collection("cities");
+
+        // Create a query against the collection.
+        Query query = citiesRef.whereEqualTo("state", "CA");
+
+        Query capitalCities = db.collection("cities").whereEqualTo("capital", true);
+
+        db.collection("cities")
+            .whereEqualTo("capital", true)
+            .get()
+            .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                @Override
+                public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                    if (task.isSuccessful()) {
+                        for (QueryDocumentSnapshot document : task.getResult()) {
+                            Toast.makeText(MainActivity.this, document.getId() + " => " + document.getData(),Toast.LENGTH_SHORT).show();
+                        }
+                    } else {
+                        Toast.makeText(MainActivity.this, "Error getting documents: "+task.getException(),Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
     }
 
     //Fetch Data from Cloud
